4a121aa830b23e771737653af519691c7019ad08
resqiar <resqiageng@gmail.com>
2023-05-19 23:05:59 +0700

    fix: fix incomplete user entity

--- entities/user.entity.go
+++ entities/user.entity.go
@@ -3,21 +3,19 @@ package entities
 import (
 	"time"
 
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid; primaryKey"`
+	ID        string `gorm:"type:uuid; primaryKey; default:gen_random_uuid()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 
-	Provider    string `gorm:"type:varchar(48)"` // Third-party provider (e.g., Google, Facebook)
-	ProviderID  string `gorm:"type:text"`        // ID provided by the third-party provider
-	AccessToken string `gorm:"type:text"`        // Access token obtained from the third-party provider
+	Provider   string `gorm:"type:varchar(48); not null"` // Third-party provider (e.g., Google, Facebook)
+	ProviderID string `gorm:"type:text"`                  // ID provided by the third-party provider
 
-	Username string `gorm:"type:varchar(100); unique"`
-	Email    string `gorm:"type:varchar(100); unique"`
+	Username string `gorm:"type:varchar(100); unique; not null"`
+	Email    string `gorm:"type:varchar(100); unique; not null"`
 	Bio      string `gorm:"type:text; nullable"`
 }
